cb8d88bdbee30e00ff20bc02ad93de504956bd16
neo <neoweight@gmail.com>
2023-12-07 20:43:26 +0000

    feat: Introduce 'Post' model and display posts on forum-home page
    - Created a new model named 'Post' in the forum app to represent posts in the forum.
    - Updated the 'forum-home' page to display all instances of the 'Post' model, showcasing all forum posts.
    - Removed dummy data from the 'forum-home' view.

--- forum/views.py
+++ forum/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Neo',
-        'title': 'Welcome to Revision Goat!',
-        'content': 'First post content',
-        'date_posted': 'November 29, 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Help me revise!!!',
-        'content': 'Second post content',
-        'date_posted': 'November 29, 2023'
-    }
-]
-
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'forum/home.html', context)
 
