f73f626ed624b5140d3400cf068a715a15755548
neo <neoweight@gmail.com>
2023-12-28 23:32:25 +0000

    feat: Enhance profile page, add profile pictures, create profiles on user signup
    - Improved the 'profile.html' page with additional features and enhanced layout.
    - Implemented the addition of profile pictures for users.
    - Updated the system to automatically create profiles upon user signup.

--- revision_site/settings.py
+++ revision_site/settings.py
@@ -141,6 +141,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
--- revision_site/urls.py
+++ revision_site/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('forum/', include('forum.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
