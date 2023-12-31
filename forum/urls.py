ef214c4bb7b35d665005cea5a6437b3c2217757f
neo <neoweight@gmail.com>
2023-12-31 18:02:56 +0000

    feat: Implement PostListView for forum homepage, add post CRUD operations
    - Updated the forum homepage to use 'PostListView' for displaying posts.
    - Added functionality to view individual posts.
    - Enabled users to create new posts.
    - Implemented the ability to update existing posts.
    - Added the option to delete posts.

--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 #urls go here budd!!
 
 urlpatterns = [
-    path('', views.home, name='forum-home'),
+    path('', views.PostListView.as_view(), name='forum-home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='forum-about'),
-    path('base/', views.base, name='forum-base'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -9,9 +11,48 @@ def home(request):
     }
     return render(request, 'forum/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'forum/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    success_url = '/'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'forum/about.html', {'title': 'About'})
-
-def base(request):
-    return render(request, 'forum/base.html', {'title': 'About'})
