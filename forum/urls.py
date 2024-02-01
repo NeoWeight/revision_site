ca76a6f1406c84be18e832753e5b1ff6a08dd3a5
neo <neoweight@gmail.com>
2024-02-01 20:42:10 +0000

    feat: Introduce pagination for forum posts, implement user profiles
    - Implemented pagination functionality for easier navigation of forum posts.
    - Introduced user profiles displaying all posts authored by the user.

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 #urls go here budd!!
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='forum-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/new', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -16,6 +19,17 @@ class PostListView(ListView):
     template_name = 'forum/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'forum/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
